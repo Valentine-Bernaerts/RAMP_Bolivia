bb6a05a4d37fed023920cf29e40157f515619294
Valentine Bernaerts <valentine.bernaerts@student.uliege.be>
2026-03-10 17:11:52 -0400

    Add water heating module for household analysis

---
+++ inputs/Norte_Amazonía/summer/households/sufficiency/water_heating.py
@@ -0,0 +1,17 @@
+from ramp.core.core import User
+import pandas as pd
+import os
+
+User_list = []
+
+HWH = User("household water heating", 1)  # Create user with ID = 1
+User_list.append(HWH)
+
+current_dir = os.path.dirname(__file__)  # /inputs/highlands/fall/households/sufficiency
+project_root = os.path.abspath(os.path.join(current_dir, "..", "..", "..", "..", ".."))  # RAMP_Bolivia
+csv_path = os.path.join(project_root, "data", "LL_power_profile_water_heating.csv")
+HH_shower_P = pd.read_csv(csv_path, sep=";", decimal=",")
+
+HWH_shower = HWH.add_appliance(1, HH_shower_P, 2, 30, 0.2, 3, thermal_p_var = 0.4)
+HWH_shower.windows([360, 540], [1080, 1200], 0.2)
+
