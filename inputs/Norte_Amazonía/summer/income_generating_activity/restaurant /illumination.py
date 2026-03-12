2c3c18b7178868fa3c94fbaadc4187c3469e6e93
Valentine Bernaerts <valentine.bernaerts@student.uliege.be>
2026-03-12 16:35:12 -0400

    Add illumination.py for managing indoor lighting

---
+++ inputs/Norte_Amazonía/summer/income_generating_activity/restaurant /illumination.py
@@ -0,0 +1,9 @@
+from ramp.core.core import User
+User_list = []
+
+
+IL = User("Illumination", 1)
+User_list.append(IL)
+
+IL_indoor_bulb = IL.add_appliance(2,7,2,120,0.2,10)
+IL_indoor_bulb.windows([1107,1440],[0,30],0.35)
