95ed4a458a5c0489412f884df30caa81eabbc63e
Valentine Bernaerts <valentine.bernaerts@student.uliege.be>
2026-03-12 16:35:26 -0400

    Add kitchen.py for kitchen user and appliances

---
+++ inputs/Norte_Amazonía/summer/income_generating_activity/restaurant /kitchen.py
@@ -0,0 +1,9 @@
+from ramp.core.core import User
+User_list = []
+
+
+K = User("Kitchen", 1)
+User_list.append(K)
+
+K_Blender = K.add_appliance(1,350,2,20,0.375,5)
+K_Blender.windows([420,480],[720,780],0.5)
