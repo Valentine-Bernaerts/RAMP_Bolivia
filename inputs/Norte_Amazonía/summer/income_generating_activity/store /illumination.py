ad7298c74bc8cb5c79638aad803fd71ced0001a1
Valentine Bernaerts <valentine.bernaerts@student.uliege.be>
2026-03-12 16:39:52 -0400

    Add illumination.py for user management and appliances

---
+++ inputs/Norte_Amazonía/summer/income_generating_activity/store /illumination.py
@@ -0,0 +1,13 @@
+from ramp.core.core import User
+User_list = []
+
+
+IL = User("Illumination", 1)
+User_list.append(IL)
+
+
+IL_indoor_bulb = IL.add_appliance(2,7,2,120,0.2,10)
+IL_indoor_bulb.windows([1107,1440],[0,30],0.35)
+
+IL_outdoor_bulb = IL.add_appliance(1,13,2,600,0.2,10)
+IL_outdoor_bulb.windows([0,330],[1107,1440],0.35)
